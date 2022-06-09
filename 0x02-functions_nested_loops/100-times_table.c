af6c6683cf90317a9343a4dd2e79f421bd8e194c
root <root@DESKTOP-8EV08QT.localdomain>
2022-06-09 15:13:47 +0100

    0. Multiple mains

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_times_table - prints the n times table, starting with 0.
+  * @n: number times table.
+  */
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return (-1);
+	else
+		
+
+}
