68a22299fb4544796d3bdb705fd57f9c8047bde9
root <root@DESKTOP-8EV08QT.localdomain>
2022-06-13 20:36:21 +0100

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * _puts - function that prints a string, followed by a new line, to stdout.
+  * @str: string to print
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	char str;
+
+	str = "Yes i can!";
+	puts(str);
+	puts('\n');
+}
