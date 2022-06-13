d27238e51a3d4ff7e88b579e90f634fac70d0a7b
root <root@DESKTOP-8EV08QT.localdomain>
2022-06-13 20:18:19 +0100

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strlen - function that returns the length of a string.
+  * @s: a string
+  * Return: returns the length of a string.
+  */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+
+}
